bef6b88b249b81873f4fcf1cb89a1d1469af1d46
franklinlem <franklinlem@yahoo.com>
2023-08-31 16:57:59 -0300

    restful

--- 01.RESTFUL/server.js
+++ 01.RESTFUL/server.js
@@ -1,5 +1,46 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const data = require("./data.json");
+
+app.use(express.json());
+
+app.get("/clients", function(req, res){
+    res.json(data);
+});
+
+app.get("/clients/:id", function(req, res){
+    const {id} = req.params
+    const client = data.find(client => client.id = id);
+
+    if(!client) return res.status(404).json();
+
+    res.json(client);
+});
+
+app.post("/clients", function(req, res){
+    const{name, email} = req.body;
+
+    res.json({name, email});
+});
+
+app.put("/clients/:id", function(req, res){
+    const{id} = req.params;
+    const client = data.find(cli => cli.id === id);
+
+    if(!client) return res.status(204).json();
+    const{name} = req.body;
+    client.name = name;
+
+    res.json(client);
+});
+
+app.delete("/clients/:id", function(req, res){}
+    const{id} = req.params;
+    const clientsFiltered = data.filter(client => client.id != id);
+
+    res.json(clientsFiltered);
+);
+
 
 app.listen(3000, function(){
     console.log('Server is running on port 3000');
