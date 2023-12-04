8c534845a005f72fc2bcdb02b7670e8514af2be4
Franklin Martins <franklinlem@yahoo.com>
2023-12-04 16:14:25 -0300

    estudando javascript

--- aula11/ex008.js
+++ aula11/ex008.js
@@ -1,7 +1,9 @@
 console.log('O console funcionou corretamente!')
-var vel = 60.5
+var vel = 50.5
 console.log(`A velocidade do seu carro é ${vel} km/h!`)
-if (vel > 60){
+if (vel > 80){
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!!!`)
+} else{
+    console.log(`Você está na velocidade permitida.`)
 }
 console.log(`Dirija sempre usando cinto de segurança!`)
