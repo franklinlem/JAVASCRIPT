6b1e8504fce09c7a3f9cd747c5fca8ed912d90ea
Franklin Martins <franklinlem@yahoo.com>
2023-12-06 02:15:58 -0300

    estudando js

---
+++ aula16/funcao05.js
@@ -0,0 +1,11 @@
+//usando recursão de função
+
+function fatorial(n){
+    if (n==1 || n==0){
+        return 1
+    } else {
+        return n * fatorial (n-1)
+    }
+}
+
+console.log(fatorial(10))
