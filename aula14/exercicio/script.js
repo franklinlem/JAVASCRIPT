98d66eedcd8f601b56cdac6f31fc3c270ac9cde3
Franklin Martins <franklinlem@yahoo.com>
2023-12-13 16:57:43 -0300

    exercicio javascript

--- aula14/exercicio/script.js
+++ aula14/exercicio/script.js
@@ -5,21 +5,26 @@ function contar(){
     var res = document.getElementById('res')
     
     if (ini.value.length==0 || fim.value.length==0 || passo.value.length==0){
-        alert('[ERRO!] Faltam dados!')
+        res.innerHTML = "Impossível contar!"
+        //alert('[ERRO!] Faltam dados!')
     } else{
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         var i = Number(ini.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
-        if(i<f){
-            //contagem crescente
-            for (let c=i; c<=f; c+=p){
-                res.innerHTML+= `${c} \u{1f449}`
-        } else{
-            //contagem regressiva
+        if (p<=0){
+            alert('Passo inválido! Considerando Passo=1')
+            p=1
         }
-        res.innerHTML+=`\u{1f3c1}`
-
+        if (i<f){
+            for (var c=i; c<=f; c+=p){
+                res.innerHTML+=`${c} \u{1f449}`
+            }
+        } else {
+            for (var c=i; c>=f; c-=p){
+                res.innerHTML+=`${c} \u{1f449}`
+            }
         }
+        res.innerHTML+= `\u{1f3c1}`
     }
 }
