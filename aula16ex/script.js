2c0ceb2b51a686ae05db47ce52ec724a1953025d
Franklin Martins <franklinlem@yahoo.com>
2023-12-18 01:10:13 -0300

    estudando javascript

--- aula16ex/script.js
+++ aula16ex/script.js
@@ -22,7 +22,25 @@ function inLista(n,l){
 function adicionar(){
     if (isNumero(num.value) && !inLista(num.value, valores)){
         valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
     } else {
         window.alert('Valor inválido ou já adicionado!')
     }
+    num.value = ''
+    num.focus()
+    
+}
+
+function finalizar(){
+    if (valores.length==0){
+        window.alert('Adicione valores antes de finalizar!')
+    }else{
+        let tot = valores.length
+
+        res.innerHTML = ''
+        res.innerHTML = `Ao todo temos ${tot} elementos cadastrados.`
+    }
+    
 }
