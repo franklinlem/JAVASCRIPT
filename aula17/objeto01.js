76a5ef8e104d67b1514bf3ef78b8fe6586218116
Franklin Martins <franklinlem@yahoo.com>
2023-12-20 11:53:23 -0300

    atualizando js

--- aula17/objeto01.js
+++ aula17/objeto01.js
@@ -1,2 +1,12 @@
-let amigo = []
-console.log(typeof amigo)
+let amigo = {
+    nome:'José',
+    sexo: 'M',
+    peso: 85.4,
+    engordar(p=0){
+        console.log('Ele(a) engordou!')
+        this.peso += p
+
+    }
+}
+amigo.engordar(2)
+console.log(`${amigo.nome} está pesando ${amigo.peso} kg.`)
