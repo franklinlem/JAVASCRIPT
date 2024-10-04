7a6a1238840dedfb66d68d4ad08679f8e54a55ee
Franklin Martins <franklinlem@yahoo.com>
2024-10-04 11:46:20 -0300

    estudando javascript nodejs express

--- requisicaoHttp/server.js
+++ requisicaoHttp/server.js
@@ -1,10 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 //app.get('/', (req, res)  => {
 //    res.sendFile(`${__dirname}/index.html`);    
 //});
 
-app.get("/criararquivo/"), (req, res) => {});
+app.get("/criararquivo/", (req, res) => {
+/*    try{
+        criarArquivo();
+        res.sendStatus(200);
+    } catch (error){
+        res.sendStatus(500);
+        console.log(error)
+    }*/
+   res.sendStatus(criarArquivo());
+});
+function criarArquivo(){
+    if (require("fs").existsSync("exemplo.txt")){
+        require("fs").writeFileSync("exemplo.txt", "insere esse texto no arquivo", "UTF-8");
+        return 200;
+    } else{
+        return 404;
+    }
+}
 
 app.listen(3000, console.log("Servidor ativo em: http://localhost:3000/"));
