48f4019509c6c3f72be949a68241297d6dcb3dd0
franklinlem <franklinlem@yahoo.com>
2024-10-18 10:09:57 -0300

    estudando nodejs

--- node/projeto/meuapp/server.js
+++ node/projeto/meuapp/server.js
@@ -4,4 +4,7 @@ app.use(express.static("wwwroot"));
 app.get('/', (req, res)  => {
     res.sendFile(`${__dirname}/index.html`);    
 });
+app.all("*", (req, res) => {
+    res.status(404).send(require("fs").readFileSync("./wwwroot/erro.html", "utf-8"));
+});
 app.listen(3000, console.log("Servidor ativo em: http://localhost:3000/"));
