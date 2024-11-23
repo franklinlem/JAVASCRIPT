4c6ff9479af1d3d4a0928411deb1f9ac8ee7adf7
franklinlem <franklinlem@yahoo.com>
2024-11-23 13:32:24 -0300

    estudando javascript

---
+++ livrojs/cap04/nodejs/node_e51c49d867b39.tmp.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")()
+const pessoas = Number(prompt("Número de pessoas: "))
+const peixes = Number(prompt("Número de peixes: "))
+let pagar = 0
+if (peixes <= pessoas){
+    pagar = pessoas * 20
+} else {
+    pagar = (pessoas * 20)+ ((peixes - pessoas) * 12)
+}
+console.log(`Pagar R$ ${pagar.toFixed(2)}`)
