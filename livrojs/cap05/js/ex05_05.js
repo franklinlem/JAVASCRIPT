60e3c7e60c30342792de6adc8af04f9ffa6fa498
franklinlem <franklinlem@yahoo.com>
2024-12-29 09:42:48 -0300

    estudando javascript

---
+++ livrojs/cap05/js/ex05_05.js
@@ -0,0 +1,22 @@
+const frm = document.querySelector('form')
+const resp1 = document.querySelector('#outResp1')
+const resp2 = document.querySelector('#outResp2')
+
+let resposta = ''
+let numContas = 0
+let valTotal = 0
+
+frm.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const descricao = frm.inDescricao.value
+  const valor = Number(frm.inValor.value)
+  numContas++
+  valTotal = valTotal + valor // valTotal += valor
+  resposta = resposta + descricao + ' - R$ ' + valor.toFixed(2) + '\n'
+  resp1.innerText = `${resposta} ---------------------------------`
+  resp2.innerText = `${numContas} Conta(s) - Total: R$ ${valTotal.toFixed(2)}`
+
+  frm.inDescricao.value = '' //limpa campo
+  frm.inValor.value = '' //limpa campo
+  frm.inDescricao.focus() //coloca o foco no campo
+  })
