c24a1b597b1c86d4ef9b7b259118ee759fc234ed
franklinlem <franklinlem@yahoo.com>
2024-12-29 10:16:18 -0300

    estudando javascript

--- livrojs/cap05/js/ex05_05.js
+++ livrojs/cap05/js/ex05_05.js
@@ -10,13 +10,16 @@ frm.addEventListener('submit', (e) => {
   e.preventDefault()
   const descricao = frm.inDescricao.value
   const valor = Number(frm.inValor.value)
+  const vencto = new Date(frm.inVencto.value)
+  let data = vencto.toLocaleDateString('pt-BR')
   numContas++
   valTotal = valTotal + valor // valTotal += valor
-  resposta = resposta + descricao + ' - R$ ' + valor.toFixed(2) + '\n'
-  resp1.innerText = `${resposta} ---------------------------------`
+  resposta = resposta + descricao + ' - R$ ' + valor.toFixed(2) + ' - Vencimento em ' + data + '\n'
+  resp1.innerText = `${resposta} --------------------------------------------`
   resp2.innerText = `${numContas} Conta(s) - Total: R$ ${valTotal.toFixed(2)}`
 
   frm.inDescricao.value = '' //limpa campo
   frm.inValor.value = '' //limpa campo
+  frm.inVencto.value = '' //limpa campo
   frm.inDescricao.focus() //coloca o foco no campo
   })
